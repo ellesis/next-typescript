6a701a4cc11fa5c6f2a3d3047a24fd8299a7a7a5
Lisa Han <ellesis@gmail.com>
2022-06-27 07:34:45 -0800

    Create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { HiSearch, HiBell } from 'react-icons/hi'
+
+function Header() {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        {/* logo */}
+        <Image
+          src="/img/netflix_2015_logo.svg"
+          width={100}
+          height={30}
+          alt="Picture of the author"
+          className="cursor-pointer object-contain"
+        />
+
+        {/* left menu */}
+        <ul className="hidden space-x-4 md:flex">
+          <li className="header-link">Home</li>
+          <li className="header-link">TV Shows</li>
+          <li className="header-link">Movies</li>
+          <li className="header-link">New & Popular</li>
+          <li className="header-link">My List</li>
+        </ul>
+      </div>
+
+      {/* right menu */}
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <HiSearch className="hidden h-6 w-6 sm:inline" />
+        <p>Kids</p>
+        <HiBell className="hidden h-6 w-6 sm:inline" />
+        <Link href="/account">
+          <img src="/img/avatar.png" width={32} height={32} alt="" className="cursor-pointer rounded" />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
