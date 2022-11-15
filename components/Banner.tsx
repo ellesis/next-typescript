b20d66a4d2138907119a0a7217c9975375f4fe83
Lisa Han <ellesis@gmail.com>
2022-11-15 09:45:48 -0800

    Create Banner component

---
+++ components/Banner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Banner() {
+  return (
+    <div>
+      {/* <Image /> */}
+      <div></div>
+    </div>
+  )
+}
+
+export default Banner
